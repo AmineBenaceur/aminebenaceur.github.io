a8aae44999312969b76c4b0ba8aeae36fab24116
AmineBenaceur <abenaceur12@gmail.com>
2021-09-10 19:31:19 -0600

    Added particle effects

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,22 @@
 import Sidebar from "./components/Sidebar";
 import styled from "styled-components";
+import HomePage from "./components/pages/HomePage";
+import { Route,Switch as Switching } from 'react-router'
+
+
 function App() {
   return (
     <div className="App">
       <Sidebar />
       <MainContentStyled>
 
+      <HomePage />
+
+      <Switching>
+        <Route path="/" exact>
+
+        </Route>
+      </Switching>
       </MainContentStyled>
     </div>
   );
@@ -15,9 +26,18 @@ const MainContentStyled = styled.main`
   position: relative;
   margin-left: 16.3rem;
   min-height: 100vh;
-  background-color: red;
+  @media screen and (max-width:1200px){
+    margin-left: 0;
+  }
+  .lines{
+    position: absolute;
+    min-height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: space-evenly;
+    opacity: 0.4;
+    z-index: -1;
 
-
+  }
 `;
-
 export default App;
--- portfolio/src/components/pages/HomePage.js
+++ portfolio/src/components/pages/HomePage.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import styled from 'styled-components';
-
+import Particle from '../Particle';
 
 function HomePage() {
     return (
         <HomePageStyled>
-            
+            <div className="particle-con">
+                <Particle />
+            </div>
         </HomePageStyled>
     );
 }
 
+
+
+
+
 const HomePageStyled = styled.header`
+    width: 100%;
+    height: 100vh;
+    position: relative;
 
 
 `;
